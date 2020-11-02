a89d857dfcbe5ebaed00f9f5619def743fb6783d
ChiehYuu <jj830502@gmail.com>
2020-11-02 23:52:29 +0800

    20201102:Book_Chapter_1 practice

---
+++ Chap_1.R
@@ -0,0 +1,21 @@
+# A vector of length 1
+x <- 3
+x
+x**2
+# A vector of length 3
+y <- c(3, 4, 5)
+y
+y**2
+#hello r & rm
+hello_msg <- 'Hello R!'
+hello_msg
+print(hello_msg)
+rm(hello_msg)
+print(hello_msg)
+#help function
+help("iris")
+help("print")
+#version detail
+sessionInfo()
+Sys.getlocale()
+getwd()
