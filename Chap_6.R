5e194b63cd71da023edbb7b0dfe7f07c33917740
ChiehYuu <jj830502@gmail.com>
2020-11-13 23:47:30 +0800

    20201113:R practice Chap.6

---
+++ Chap_6.R
@@ -0,0 +1,24 @@
+# function build
+#my_function(my_inpus, my_args, ...)
+
+#abs() 絕對值
+-5:-1
+abs(-5:-1)
+
+#sqrt() 根號
+1:5
+sqrt(1:5)
+
+#ceiling() 無條件進位
+ceiling(9.527)
+nums <- sqrt(c(7, 17, 27, 37, 47))
+nums
+ceiling(nums)
+
+#floor() 無條件去位
+floor(9.527)
+nums <- sqrt(c(7, 17, 27, 37, 47))
+nums
+floor(nums)
+
+#
