79d58569d0d9de117f169c9f828542b725081067
ChiehYuu <jj830502@gmail.com>
2020-11-22 11:50:37 +0800

    20201121:R practice Chap.12 complete + Chap.13

--- Chap_12.R
+++ Chap_12.R
@@ -47,4 +47,59 @@ View(chicago_bulls_wide)
 chicago_bulls_long <- gather(chicago_bulls_wide, key = 'pos_recoded', value = ' Wt_recoded', chicago_bulls_wide$Heavy, chicago_bulls_wide$Thin)
 View(chicago_bulls_long)
 
+#row bind
+teams <- c('Chicago Bulls', 'Chicago Bulls')
+game_results <- c('win', 'loss')
+games <- c(72, 10)
+bulls <- data.frame(team = teams, game_result = game_results, game = games)
+teams <- c('Golden State Warriors', 'Golden State Warriors')
+game_results <- c('win', 'loss')
+games <- c(73, 9)
+warriors <- data.frame(team = teams, game_result = game_results, game = games)
+bulls
+warriors
+rbind(bulls, warriors)
 
+#dplyr
+bind_rows(bulls, warriors)
+
+#col bind
+chicago_bulls_players <- chicago_bulls[, c('No.', 'Player')]
+chicago_bulls_ht_wt <- chicago_bulls[, c('Wt', 'Ht')]
+cbind(chicago_bulls_players, chicago_bulls_ht_wt)
+
+#dplyr
+bind_cols(chicago_bulls_players, chicago_bulls_ht_wt)
+
+#merge
+name <- c('Ruffy', 'Zoro', 'Nami', 'Joba')
+age <- c(19, 21, 20, 17)
+left_df <- data.frame(name, age)
+
+name <- c('Ruffy', 'Joba', 'Robin')
+devil_fruit <- c('Plastic', 'Human', 'Flower')
+right_df <- data.frame(name, devil_fruit)
+
+merge(left_df, right_df)
+
+#inner join
+inner_join(left_df, right_df)
+
+#merge index all.x
+merge(left_df, right_df, all.x = TRUE)
+
+#left join
+left_join(left_df, right_df)
+
+#merge index all.y
+merge(left_df, right_df, all.y = TRUE)
+
+#right join
+right_join(left_df, right_df)
+
+#merge index x,y
+merge(left_df, right_df, all.x = TRUE, all.y = TRUE)
+merge(left_df, right_df, all = TRUE)
+
+#full join 
+full_join(left_df, right_df)
